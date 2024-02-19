a3f2d9405912dd7bcb3866138544a909bb04847b
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:49:52 -0800

    Add files via upload

---
+++ Magicians.js
@@ -0,0 +1,12 @@
+"use strict";
+//Written By : TAYYABA RAO
+//Date : 18/feb/2024
+//Roll no : 00388416
+//array magicians
+const magicians = ["Nimra", "Irsa", "Khadija", "Hafsa"];
+function show_magicians(magicians) {
+    magicians.forEach(magicians => {
+        console.log(magicians);
+    });
+}
+show_magicians(magicians);
---
+++ Magicians.ts
@@ -0,0 +1,14 @@
+//Written By : TAYYABA RAO
+//Date : 18/feb/2024
+//Roll no : 00388416
+
+//array magicians
+const magicians:string[] = ["Nimra","Irsa","Khadija","Hafsa"];
+
+function show_magicians(magicians: string[]): void{
+    magicians.forEach(magicians  => {
+        console.log(magicians);
+    });
+}
+
+show_magicians(magicians);
